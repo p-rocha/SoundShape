da711b652bdf87a846a884de543415f226dcd6a6
Pedro Carvalho Rocha <n10832777@qut.edu.au>
2023-02-21 15:56:28 +1000

    refined the codes behind raven.to.wave function

--- R/raven.to.wave.R
+++ R/raven.to.wave.R
@@ -2,13 +2,12 @@
 #'
 #' @description create one \code{".wav"} file for each selection created using \href{https://ravensoundsoftware.com/software/raven-pro/}{Raven Pro} software, which is commonplace in bioacoustical analysis. Each selection (i.e. line in table) should represent a each acoustic unit within the sample study.
 #'
-#' @param orig.wav filepath to the folder where original \code{".wav"} files are stored. Should be presented between quotation marks. By default: \code{orig.wav = NULL} (i.e. user must specify the filepath to \code{".wav"} files)
+#' @param orig.wav.folder filepath to the folder where original \code{".wav"} files are stored. Should be presented between quotation marks. By default: \code{orig.wav.folder = NULL} (i.e. user must specify the filepath to \code{".wav"} files)
 #'
-#' @param raven.at filepath to the folder where selection tables from \href{https://ravensoundsoftware.com/software/raven-pro/}{Raven Pro} software are stored. Should be presented between quotation marks. File name should end with \code{"selections.txt"}. By default: \code{raven.at = orig.wav} (i.e. raven tables stored in the same folder as original  \code{".wav"} files)
+#' @param raven.at filepath to the folder where selection tables from \href{https://ravensoundsoftware.com/software/raven-pro/}{Raven Pro} software are stored. Should be presented between quotation marks. File name should end with \code{"selections.txt"}. By default: \code{raven.at = orig.wav.folder} (i.e. raven tables stored in the same folder as original  \code{".wav"} files)
 #'
-#' @param wav.samples name of the folder where new \code{".wav"} files will be stored. Should be presented between quotation marks. By default: \code{wav.samples = "wav samples"}
+#' @param wav.samples folder where new \code{".wav"} files will be stored. Can be either a filepath to the intended folder, or the name of the folder i.e.(\code{wav.samples = "wav samples"}). In the later case, a new folder will be created within the one specified by \code{orig.wav.folder}. Should be presented between quotation marks. By default: \code{wav.samples = "wav samples"}
 #'
-#' @param max.dur optional argument to specify intended duration of sample \code{".wav"} files. If \code{max.dur=NULL}, function \code{raven.to.wav} will define the duration as 20% longer than the maximum duration of selections from Raven Pro software. By default: \code{max.dur=NULL}
 #'
 #'
 #' @author
@@ -37,12 +36,12 @@
 #'
 #' @export
 #'
-raven.to.wave <- function(orig.wav=NULL, raven.at=orig.wav, wav.samples="wav samples", max.dur=NULL){
+raven.to.wave <- function(orig.wav.folder=NULL, raven.at=orig.wav.folder, wav.samples="wav samples"){
 
-  if(is.null(orig.wav)) {stop("Use 'orig.wav' to specify folder path where original '.wav' files are stored")}
+  if(is.null(orig.wav.folder)) {stop("Use 'orig.wav.folder' to specify folder path where original '.wav' files are stored")}
 
   # List ".wav" files
-  wav.files <- dir(orig.wav, pattern=".wav")
+  wav.files <- dir(orig.wav.folder, pattern=".wav")
 
   # List Raven tables
   raven.tables <- dir(raven.at, pattern="selections.txt")
@@ -56,51 +55,57 @@ raven.to.wave <- function(orig.wav=NULL, raven.at=orig.wav, wav.samples="wav sam
   }
 
 
-  # Import Raven tables
-  raven.selections <- data.frame()
-  for(raven in raven.tables){
-    raven.temp <- read.table(file.path(raven.at, raven),
-                             h=T, sep="\t", stringsAsFactors = T)
-
-    raven.selections <- rbind(raven.selections, raven.temp)
-
-    rm(raven.temp)
-  } # import raven tables loop
-
-
-  # Use delta time for maximum duration
-  if(is.null(max.dur)){
-    #longest acoustic unit plus 20% of longest duration
-    max.dur <- max(raven.selections$End.Time..s.-raven.selections$Begin.Time..s.)*1.2  } # max.dur
-
 
   # Create folder to store sample wav files
-  if(!dir.exists(file.path(orig.wav, wav.samples)))
-    dir.create(file.path(orig.wav, wav.samples))
+  if(!dir.exists(wav.samples) &
+     !dir.exists(file.path(orig.wav.folder, wav.samples)))
+    dir.create(file.path(orig.wav.folder, wav.samples))
 
 
   # For each wav file, use Raven selections to create new files
   for(wav in wav.files){
 
-    raven.temp <- read.table(file.path(orig.wav,
+    raven.temp <- read.table(file.path(orig.wav.folder,
                                        grep(stringr::str_sub(wav,start=0, end = -5),
                                             raven.tables, value=T)), h=T, sep="\t")
+    # Calculate duration (delta time)
+    raven.temp$Delta.Time <- raven.temp$End.Time..s.-raven.temp$Begin.Time..s.
 
     for(i in 1:length(raven.temp$Selection[raven.temp$View=="Waveform 1"])){
 
-      wav.temp <- tuneR::readWave(file.path(orig.wav, wav), units="seconds",
+      wav.temp <- tuneR::readWave(file.path(orig.wav.folder, wav), units="seconds",
                                   from= raven.temp$Begin.Time..s.[
-                                    raven.temp$Selection== i & raven.temp$View=="Waveform 1"],
+                                    raven.temp$Selection== i &
+                                      raven.temp$View=="Waveform 1"] -
+                                    raven.temp$Delta.Time[
+                                      raven.temp$Selection== i &
+                                        raven.temp$View=="Waveform 1"]*0.15,
                                   to= raven.temp$Begin.Time..s.[
-                                    raven.temp$Selection== i & raven.temp$View=="Waveform 1"]+
-                                    max.dur)
-      tuneR::writeWave(wav.temp, extensible = T,
-                       filename = file.path(orig.wav, wav.samples,
-                                            ifelse(i<10,
-                                                   paste(stringr::str_sub(wav,start=0, end = -5),
-                                                         "_sample-0", i, ".wav", sep=""),
-                                                   paste(stringr::str_sub(wav,start=0, end = -5),
-                                                         "_sample-", i, ".wav", sep=""))))
+                                    raven.temp$Selection== i &
+                                      raven.temp$View=="Waveform 1"] +
+                                    raven.temp$Delta.Time[
+                                      raven.temp$Selection== i &
+                                        raven.temp$View=="Waveform 1"]*1.15)
+
+
+      if(dir.exists(wav.samples)){
+        tuneR::writeWave(wav.temp, extensible = T,
+                         filename=file.path(wav.samples, ifelse(i<10,
+                                                                paste(stringr::str_sub(wav,start=0, end = -5),
+                                                                      "_sample-0", i, ".wav", sep=""),
+                                                                paste(stringr::str_sub(wav,start=0, end = -5),
+                                                                      "_sample-", i, ".wav", sep="")))) }
+
+      if(!dir.exists(wav.samples)){
+        tuneR::writeWave(wav.temp, extensible = T,
+                         filename = file.path(orig.wav.folder, wav.samples,
+                                              ifelse(i<10,
+                                                     paste(stringr::str_sub(wav,start=0, end = -5),
+                                                           "_sample-0", i, ".wav", sep=""),
+                                                     paste(stringr::str_sub(wav,start=0, end = -5),
+                                                           "_sample-", i, ".wav", sep=""))))  }
+
+
       rm(wav.temp)
 
     } # end loop - for each selection
@@ -109,6 +114,6 @@ raven.to.wave <- function(orig.wav=NULL, raven.at=orig.wav, wav.samples="wav sam
 
   } # end loop - for each wav file
 
-  rm(wav.files, raven.tables, raven.selections)
+  rm(wav.files, raven.tables)
 
 } # end function
